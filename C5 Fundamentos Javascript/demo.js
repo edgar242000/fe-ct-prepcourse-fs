296b15b86c9c26d48d2a7f3e837ab7899f9961a4
edgar <24.edgar@gmail.com>
2024-11-18 21:19:33 -0500

    fe-ct-prepcourse-fs

---
+++ C5 Fundamentos Javascript/demo.js
@@ -0,0 +1,25 @@
+//console.log(5 + 4 * 2 + 1 * 4 - 3);
+var indefinido = undefined
+var nombreCompleto = "edgar rivera"
+var numero1 = 1234435435
+var numero2 = -8209354530453
+var numero3 = - 535.54665867
+var booleane1 = true
+var booleane2 = false
+var nulo = null
+ console.log(nombreCompleto)
+console.log("todo lo que yo cree lo va a envìar a la consola")
+
+
+let numeroDeVentas = 541
+
+ console.log(numeroDeVentas)
+
+ numeroDeVentas = true
+
+console.log(numeroDeVentas)
+
+
+const pi = 3.1415169
+
+console.log(pi)
---
+++ C6 Bucles/bucles.js
@@ -0,0 +1,15 @@
+var suma = 0;
+
+//for (var  i=0; i< 10; i ++ ){
+//    suma = suma + 1;
+ //   console.log(suma);
+//}
+
+
+
+for (var  i=0 ; i< 5 ; i ++ ){
+    suma = suma + i;
+    console.log("variable de interacion:  ", i);
+}
+
+console.log("Variable Suma:  ", suma)
